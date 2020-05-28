1777e01d3ad480789203178a9a437e615d18d842
Edison Abdiel <43752457+edisonabdiel@users.noreply.github.com>
2020-05-28 19:13:21 +0200

    Components (#2)
    * Create basic structure Set enviorment for axios
    
    * Work in progress

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Grid } from '@material-ui/core';
 
-import {SearchBar, VideoDetail} from './components';
+import { SearchBar, VideoDetail, VideoList } from './components';
 
 import youtube from './api/youtube';
 
@@ -11,28 +11,36 @@ import youtube from './api/youtube';
 class App extends React.Component {
 
   state = {
-    video: [],
+    videos: [],
     selectedVideo: null,
-}
+  }
+
+  componentDidMount() {
+    this.handleSubmit('home page')
+  }
+  
+  onVideoSelect = (video) => {
+    this.setState ( {selectedVideo: video} )
+  }
 
   handleSubmit = async (searchTerm) => {
     const response = await youtube.get('search', {
       params: {
         part: 'snippet',
         maxResults: 5,
-        key: 'AIzaSyD2myeYeBs1EsEeqn2ukCwpLkqBt8fuDxg',
+        key: 'AIzaSyDKGGM394J5McWNEB1wWOpaJuNd5hWb5c0',
         q: searchTerm,
       }
     });
     this.setState({
-      video: response.data.items,
+      videos: response.data.items,
       selectedVideo: response.data.items[0]
     });
 
   }
 
   render() {
-    const { selectedVideo } = this.state;
+    const { selectedVideo, videos } = this.state;
 
     return (
       <Grid justify="center" container spacing={10}>
@@ -45,7 +53,7 @@ class App extends React.Component {
               <VideoDetail videos={selectedVideo}/>
             </Grid>
             <Grid item xs={4}>
-              {/* {video list} */}
+              <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
             </Grid>
           </Grid>
         </Grid>
