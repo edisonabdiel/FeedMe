0b34965938cb991709353a2d17cafb260fdddcf1
Edison Abdiel <edison.abdiel@soundcloud.com>
2020-05-28 19:10:28 +0200

    Work in progress

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,6 +7,8 @@ const VideoDetail = ({ video }) => {
     
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
 
+    console.log(video.id.videoId)
+
     return (
         <React.Fragment>
             <Paper elevation={6} style={{ height: '70%' }}>
@@ -15,7 +17,7 @@ const VideoDetail = ({ video }) => {
             <Paper elevation={6} style={{ padding: '15px' }}>
                 <Typography variant="h4">{video.snippet.title} - {video.snippet.channelTitle}</Typography>
                 <Typography variant="subtitle1">{video.snippet.channelTitle}</Typography>
-                <Typography variant="subtitle2">{}video.snippet.description</Typography>
+                <Typography variant="subtitle2">{video.snippet.description}</Typography>
             </Paper>
         </React.Fragment>
     )
